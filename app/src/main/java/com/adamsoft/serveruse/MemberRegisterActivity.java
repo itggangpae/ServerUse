e0849f76a295a4d05dbd6f478527994bd8fb58ff
itggangpae <ggangpae1@gmail.com>
2022-06-22 16:43:13 +0900

    Initial commit

---
+++ app/src/main/java/com/adamsoft/serveruse/MemberRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.adamsoft.serveruse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_register);
+    }
+}
